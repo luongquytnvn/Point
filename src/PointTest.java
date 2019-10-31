1c93142ce7835e5e0d69d094b2de7eb4efc1e32a
luongquytnvn <luongquytnvn@gmail.com>
2019-10-31 15:34:47 +0700

    a

---
+++ src/PointTest.java
@@ -0,0 +1,8 @@
+public class PointTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2,3);
+        System.out.println(point2D.toString());
+        Point3D point3D = new Point3D(2,3,4);
+        System.out.println(point3D.toString());
+    }
+}
